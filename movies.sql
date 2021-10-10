f9fd60dcc9002837e4b959256ad661676b3c5d59
Vaidik S Prabhu <72367112+VaidikSPrabhu@users.noreply.github.com>
2021-10-10 07:02:04 +0530

    Update movies.sql

--- movies.sql
+++ movies.sql
@@ -1,15 +1,22 @@
--- Creating table named as Movies --
+--Query to create database
+CREATE DATABASE movie_db;
+
+-- Query to use the selected database
+USE movie_db;
+
+-- Creating table named as Movies
 
 CREATE TABLE `movies` (
   `name` varchar(64) NOT NULL,
   `actor` varchar(64) NOT NULL,
   `actress` varchar(64) NOT NULL,
   `director` varchar(64) NOT NULL,
-  `year_of_release` int(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `year_of_release` int(4) NOT NULL,
+  PRIMARY KEY (`name`)
+);
 
 
--- Inserting values into table --
+-- Inserting values into table
 
 INSERT INTO `movies` (`name`, `actor`, `actress`, `director`, `year_of_release`) VALUES ('RAAJAKUMARA', 'PUNEETH RAJKUMAR', 'PRIYA ANAND', 'SANTHOSH ANANDDRAM', '2017');
 INSERT INTO `movies` (`name`, `actor`, `actress`, `director`, `year_of_release`) VALUES ('MUNGARUMALE', 'GANESH', 'POOJA GANDHI', 'YOGRAJ BHAT', '2006');
@@ -28,15 +35,34 @@ INSERT INTO `movies` (`name`, `actor`, `actress`, `director`, `year_of_release`)
 INSERT INTO `movies` (`name`, `actor`, `actress`, `director`, `year_of_release`) VALUES ('ROBERRT', 'DARSHAN THOOGUDEEPA', 'ASHA BHAT', 'THARUN SUDHIR', '2021');
 
 
--- Queries to fetch data from table --
+-- Queries to fetch data from table
 
+-- Retrieving all the attributes from 'movies' table
 SELECT * FROM MOVIES;
+
+-- Retrieving actor names
 SELECT DISTINCT actor FROM `movies` ORDER BY actor;
+
+-- Retrieving actress names
 SELECT DISTINCT actress FROM `movies` ORDER BY actress;
+
+-- Retrieving movie name and director of movie, based on the actor
 SELECT name, director FROM `movies` WHERE actor = 'darshan thoogudeepa';
+
+--Retrieving attributes of movies which are released after the year 2020
 SELECT name, actor, actress, director FROM `movies` WHERE year_of_release > 2020;
+
+-- Retrieving all the attributes of movie based on the movie name
 SELECT * FROM `movies` WHERE name = 'FREE GUY';
+
+-- Retrieving number of movies relased in the year 2019
 SELECT count(*) AS Movies_2019 FROM `movies` WHERE year_of_release = 2019;
+
+-- Retrieving attributes of movies and arranging the output with respect to director names in ascending order
 SELECT name, director, year_of_release FROM `movies` ORDER BY director;
+
+-- Retrieving attributes of movies which are released in between the year 2015 and 2020 and the output is arranged with respect to year of release in descending order
 SELECT name, actor, actress, year_of_release FROM `movies` WHERE year_of_release BETWEEN 2015 AND 2020 ORDER BY year_of_release DESC;
+
+-- Retrieving attributes of movies which are not released in the year 2021 and output is arranged with respect year of release in ascending order
 SELECT name, year_of_release FROM `movies` WHERE year_of_release <> 2021 ORDER BY year_of_release;
